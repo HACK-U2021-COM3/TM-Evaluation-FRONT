451518c8dd258661348229d652477b08dcb4ce16
tamemoto <tamemotoyou@gmail.com>
2021-09-09 13:35:11 +0900

    fix: line color

--- src/components/Home/map/HomeGuestMapContent.tsx
+++ src/components/Home/map/HomeGuestMapContent.tsx
@@ -7,7 +7,6 @@ import { measureResponseType } from "lib/models/measure";
 import { decordMap } from "lib/util/map-decode";
 
 
-const center = {lat: 35.02664,lng: 136.622259}
 
 
 const HomeMapGuestContentComponent: React.VFC<{
@@ -17,13 +16,20 @@ const HomeMapGuestContentComponent: React.VFC<{
     routes: measureResponseType[]
 }> = ({addRoutesPoint, settingLocation, resultLocations, routes}) => {
     const [selected, setSelected] = useState<searchResponseType | null>(null)
-    
+    let center = {lat: 35.02664,lng: 136.622259}
+    let zoom = 8
+    const hitLocation = resultLocations.find(_=>_)?.location
+    if(!!hitLocation) {
+        center = hitLocation
+        zoom = 13
+    }
+
     return(
         <LoadScript googleMapsApiKey={`${process.env.REACT_APP_GOOGLE_MAP_API_KEY}`}>
             <GoogleMap
             mapContainerStyle={{ height: "calc(100% - 112px)", borderRadius: "10px"}}
             center={center}
-            zoom={8}
+            zoom={zoom}
             >
             {resultLocations.map((marker: searchResponseType, i: number) => (
                 <Marker 
@@ -36,7 +42,13 @@ const HomeMapGuestContentComponent: React.VFC<{
                 <Marker 
                     position={marker.start_location}
                 />
-                <Polyline path={decordMap(marker.routes_points, 5)} />
+                <Polyline
+                options={{
+                    strokeColor: "#FF0000",
+                    strokeOpacity: 0.8,
+                }}
+                path={decordMap(marker.routes_points, 5)}
+                />
                 </Fragment>
              ))}
             {!!selected && (
