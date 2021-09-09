760c5f05698c046d46176687e51836c1f2d759bb
Yum-git <yzk.yuma@gmail.com>
2021-09-10 04:02:01 +0900

    Fix 経路と地点を分離させるように変更

--- src/lib/hooks/useMeasure.ts
+++ src/lib/hooks/useMeasure.ts
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react"
 import { MeasureService } from "lib/services/MeasureService"
 import { measureResponseType, measuseRequestType} from "lib/models/measure"
+import { pointResponseType, measureFixResponseType } from "../models/measure_point";
 
 const useMeasure = (form: measuseRequestType) => {
-    const [measureResults, setMeasureResults] = useState<measureResponseType[]>([])
+    // const [measureResults, setMeasureResults] = useState<measureResponseType[]>([])
+    const [measureResults, setMeasureResults] = useState<measureFixResponseType[]>([])
+    const [pointResults, setPointResults] = useState<pointResponseType[]>([])
     const [measureLoading, setMeasureLoading] = useState<boolean>(false)
     const [error, setError] = useState<Error | null>(null)
     useEffect(() => {
@@ -12,19 +15,107 @@ const useMeasure = (form: measuseRequestType) => {
                 try {
                     setMeasureLoading(true)
                     const res = await (new MeasureService()).getMeasureLocations(form)
-                    setMeasureResults(res)
+
+                    // 経路 -> polyline・距離・移動時間
+                    let measure_list: measureFixResponseType[] = [];
+                    // 場所 -> 座標・住所・待機時間
+                    let point_list: pointResponseType[] = [];
+
+                    // lengthが1の時
+                    if(res.length === 1){
+                        let routes_item = res[0];
+
+                        // points
+                        let start_dict_point = {
+                            location: routes_item.start_location,
+                            address: routes_item.start_address,
+                            stay_time: routes_item.start_stay_time
+                        };
+                        let end_dict_point = {
+                            location: routes_item.end_location,
+                            address: routes_item.end_address,
+                            stay_time: routes_item.end_stay_time
+                        };
+
+                        point_list.push(start_dict_point);
+                        point_list.push(end_dict_point);
+
+                        // measures
+                        let start_dict_measure = {
+                            routes_points: routes_item.routes_points,
+                            distance: routes_item.distance,
+                            duration: routes_item.duration
+                        };
+
+                        measure_list.push(start_dict_measure);
+                    }else {
+                        for (let idx = 0; idx < res.length; ++idx){
+                            let routes_item = res[idx];
+                            if(idx !== res.length - 1){
+                                let dict_point = {
+                                    location: routes_item.start_location,
+                                    address: routes_item.start_address,
+                                    stay_time: routes_item.start_stay_time
+                                };
+
+                                point_list.push(dict_point);
+
+                                let dict_measure = {
+                                    routes_points: routes_item.routes_points,
+                                    distance: routes_item.distance,
+                                    duration: routes_item.duration
+                                };
+
+                                measure_list.push(dict_measure);
+                            }else{
+                                let start_dict_point = {
+                                    location: routes_item.start_location,
+                                    address: routes_item.start_address,
+                                    stay_time: routes_item.start_stay_time
+                                };
+
+                                point_list.push(start_dict_point);
+
+                                let end_dict_point = {
+                                    location: routes_item.end_location,
+                                    address: routes_item.end_address,
+                                    stay_time: routes_item.end_stay_time
+                                };
+
+                                point_list.push(end_dict_point);
+
+                                let dict_measure = {
+                                    routes_points: routes_item.routes_points,
+                                    distance: routes_item.distance,
+                                    duration: routes_item.duration
+                                };
+
+                                measure_list.push(dict_measure);
+                            }
+                        }
+                    }
+                    console.log("###########################");
+                    console.log(point_list, "point");
+                    console.log(measure_list, "measure");
+                    console.log("###########################");
+
+
+                    setPointResults(point_list)
+                    setMeasureResults(measure_list)
+                    // setMeasureResults(res)
                     setError(null)
                 } catch(e) {
                     setError(e as Error)
                 }
-                setMeasureLoading(false)    
+                setMeasureLoading(false)
             } else {
                 setMeasureResults([])
             }
         }
         void load()
     }, [form])
-    return { measureResults, measureLoading, error }
+    // return { measureResults, measureLoading, error }
+    return {measureResults, pointResults, measureLoading, error }
 }
 
 export default useMeasure;
