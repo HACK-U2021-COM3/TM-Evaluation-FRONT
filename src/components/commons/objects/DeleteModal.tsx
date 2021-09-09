038c99595d7aba6997b2d6158078e3275293f199
tamemoto <tamemotoyou@gmail.com>
2021-09-09 14:34:07 +0900

    feat delete handler

---
+++ src/components/commons/objects/DeleteModal.tsx
@@ -0,0 +1,59 @@
+import React from "react"
+import { useHistory } from "react-router";
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+  } from "@chakra-ui/react";
+import { PlansService } from "lib/services/PlansService";
+
+type Props = {
+  planId: string,
+  isOpen: boolean
+  onClose: () => void
+}
+
+const DeleteModalComponent: React.FC<Props> = ({
+  children,
+  planId,
+  isOpen,
+  onClose,
+}) => {
+    const history = useHistory()
+    const deletePlanHandler = async () => {
+      await (new PlansService()).deletePlan(planId)
+      history.go(0)
+      onClose()
+    }
+    return (
+      <>
+        {children}
+        <Modal
+          isOpen={isOpen}
+          onClose={onClose}
+          isCentered
+        >
+          <ModalOverlay background="rgba(0,0,0,0.7)"/>
+          <ModalContent>
+            <ModalHeader>予定の削除</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              この予定を削除しますか?
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={deletePlanHandler} colorScheme="red" mr={3}>
+                削除
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
+
+  export default DeleteModalComponent;
